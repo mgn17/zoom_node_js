479ed34646c213a92b92ebcc72d251405595d5b7
Mark Nourallah <mgn17@maill.aub.edu>
2021-01-19 00:13:26 +0200

    Online fix test 2

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: 'https://zoom-nodejs.onrender.com/',
+    host: '/',
     port: '443'
 })
 const myVideo = document.createElement('video')
