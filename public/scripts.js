b3fb328aca3df95d6c29f7a6813c49352d5c82e2
mgn17 <49205277+mgn17@users.noreply.github.com>
2021-01-19 02:07:26 +0200

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     secure: true,
-    host: 'https://zoom-nodejs.onrender.com',
+    host: '/',
     port: '443',
 
 })
